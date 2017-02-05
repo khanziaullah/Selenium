ca9340113afbaaf478d3bdbd45ed2a7040a93c8a
Ziaullah Khan <khan.ziaullah@gmail.com>
2017-02-05 15:53:51 -0500

    Firefox Driver example.

---
+++ WebDriverDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace WebDriverDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firefoxDriverService = FirefoxDriverService.CreateDefaultService();
+            var driver = new FirefoxDriver(firefoxDriverService);
+            driver.Url = "http://www.google.com/";
+        }
+    }
+}
