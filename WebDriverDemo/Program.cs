0f2d30f22e3488ce153c57c9a32a1a95b4b98d3f
Ziaullah Khan <khan.ziaullah@gmail.com>
2017-02-05 17:08:59 -0500

    Added code for FindELement/FindElements

--- WebDriverDemo/Program.cs
+++ WebDriverDemo/Program.cs
@@ -8,6 +8,8 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 
+using System.Linq;
+
 namespace WebDriverDemo
 {
     class Program
@@ -17,6 +19,15 @@ namespace WebDriverDemo
             var firefoxDriverService = FirefoxDriverService.CreateDefaultService();
             var driver = new FirefoxDriver(firefoxDriverService);
             driver.Url = "http://www.google.com/";
+
+            var searchBox = driver.FindElement(By.Id("lst-ib"));
+            searchBox.SendKeys("Ziaullah Khan Github");
+            searchBox.Submit();
+
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+
+            var imagesLink = driver.FindElements(By.ClassName("r"))[0];
+            imagesLink.Click();
         }
     }
 }
