08a6d9c1d8d63a15c1a1b4408fde76a841cefeef
Ziaullah Khan <khan.ziaullah@gmail.com>
2017-02-06 23:00:21 -0500

    Added Select element examples

---
+++ WebDriverSelectors/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebDriverSelectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = FirefoxDriverService.CreateDefaultService();
+            var driver = new FirefoxDriver(service);
+            var notExpected = string.Empty;
+
+            driver.Url = @"file:///C:/Code/Selenium/WebDriverDemo/WebDriverSelectors/SelectOptionPage.html";
+            var colorOption = driver.FindElement(By.Id("color1"));
+
+            var colorSelectOption = new SelectElement(colorOption);
+
+            colorSelectOption.SelectByValue("yellow");
+            colorOption.Click();
+        }
+
+        static void SelectOptionOldWay(string[] args)
+        {
+            // Arrange
+            var service = FirefoxDriverService.CreateDefaultService();
+            var driver = new FirefoxDriver(service);
+            var notExpected = string.Empty;
+
+            // Act
+            driver.Url = @"file:///C:/Code/Selenium/WebDriverDemo/WebDriverSelectors/SelectOptionPage.html";
+            var colorOption = driver.FindElement(By.Id("color1"));
+            var actual = colorOption.GetAttribute("value");
+
+            // Assert
+            Assert.AreNotEqual(actual, notExpected);
+        }
+
+        static void RadioButton(string[] args)
+        {
+
+            var service = FirefoxDriverService.CreateDefaultService();
+            var driver = new FirefoxDriver(service);
+            driver.Url = @"file:///C:/Code/Selenium/WebDriverDemo/WebDriverSelectors/RadioButtonPage.html";
+
+            var radioButton = driver.FindElements(By.Name("color"))[1];
+            radioButton.Click();
+
+
+        }
+    }
+}
+
